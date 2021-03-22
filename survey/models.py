f21e58b5c39098415aff232eedd7525a5798a876
Kevin Rojas <kevin.ricardo.rojas@gmail.com>
2021-03-22 14:34:16 -0500

    update survey and ending control

--- survey/models.py
+++ survey/models.py
@@ -18,40 +18,40 @@ class Group(BaseGroup):
 class Player(BasePlayer):
 
     age = models.IntegerField(
-        label='What is your age?',
+        label='나이(만 ____세):',
         min=13, max=125)
 
     gender = models.StringField(
-        choices=['Male', 'Female', 'Other'],
-        label='What is your gender?',
+        choices=['남성 ', '여성', '기타'],
+        label='성별:',
         widget=widgets.RadioSelect)
 
     education = models.StringField(
-        choices=['No schooling','Primary School','Middle School','High School',
-        'Some College','Trade/Technical/Vocational Training','Associate Degree',
-        'Bachelor\'s Degree','Master\'s Degree','Doctorate'],
-        label='What is your highest level of education attained?',
+        choices=['무학','초등학교 졸업','중학교 졸업','고등학교 졸업',
+        '대학교 재학 또는 중퇴','무역./기술/직업 교육','2년제 대학 학위',
+        '4년제 대학 학위','석사 학위','박사 학위'],
+        label='당신의 최종 학력은 무엇입니까?',
         widget=widgets.RadioSelect)
 
     language = models.StringField(
-        label='What is your native language?',
+        label='모국어',
         widget=widgets.TextInput)
 
     country = models.StringField(
-        label='In which country were you born?',
+        label='태어난 나라',
         widget=widgets.TextInput)
 
     intent = models.StringField(
-        choices=['Selfish','Generous','Hostile','Cooperative','Rational','Irrational'],
-        label='What do you think the intent of the other player was in their decisions?',
+        choices=['이기적','관대한','적대적','협력적','이성적','비이성적'],
+        label='귀하는 상대방 참가자가 결정을 할 때 어떤 의도를 지니고 있었다고 생각하십니까?',
         widget=widgets.RadioSelect)
 
     identity = models.StringField(
-        choices=['Yes','No','Maybe'],
-        label='If you knew the identity of the other player, would you have made a different decision?',
+        choices=['예','아니오','아마도'],
+        label='만약 귀하가 같이 게임을 한 상대방 참가자가 누구인지 알았다면, 다른 선택을 내렸을 것이라고 생각하십니까?',
         widget=widgets.RadioSelect)
 
     risk = models.StringField(
-        choices=['0 risk averse','1','2','3','4','5','6','7','8','9','10 fully prepared to take risks'],
-        label='How do you rate yourself personally? In general, are you someone who is ready to take risks or do you try to avoid risks? \n Please provide your answers using the scale provided again. 0 means “Not prepared to take risks at all”. 10 means “Prepared to take risks”. You can use the in-between ratings to tailor your response.',        
+        choices=['0 – 위험을 회피하는','1','2','3','4','5','6','7','8','9','10 – 위험을 감수할 준비가 충분히 되어 있음'],
+        label='귀하는 자신을 개인적으로 어떻게 평가하십니까? 일반적으로 기꺼이 위험을 감수하는 사람입니까, 아니면 위험을 피하려고 합니까? 제공된 척도를 사용하여 답변을 해 주십시오. 0은 "전혀 위험을 감수할 준비가 되어 있지 않음"을 의미합니다. 10은 "위험을 감수할 준비가 되어 있음"을 의미합니다. 귀하는 0-10 사이의 숫자를 사용하여 답변할 수 있습니다.',
         widget=widgets.RadioSelect)
