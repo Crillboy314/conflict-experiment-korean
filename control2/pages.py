4c7364d02ecc5eb35262a5ec1892b2339a26f59f
Diego M <de.diego.12@hotmail.com>
2021-09-27 16:48:55 -0500

    Fixed typos correction from Minha, revised for compatibility with otree < 5

--- control2/pages.py
+++ control2/pages.py
@@ -40,8 +40,8 @@ class RLC_P1(Page):
 
     def send_message_choices(self):
         choices = [
-                ['LC', '나는' + Constants.P1_codified_L + '를 선택합니다.'],
-                ['RC', '나는' + Constants.P1_codified_R + '를 선택합니다.']
+                ['LC', '나는 ' + Constants.P1_codified_L + '를 선택합니다.'],
+                ['RC', '나는 ' + Constants.P1_codified_R + '를 선택합니다.']
         ]
         if not self.group.ask_used:
             choices.append(
@@ -72,8 +72,8 @@ class RLC_P2(Page):
 
     def send_answer_choices(self):
         choices = [
-                ['LC', '나는' + Constants.P2_codified_L + '를 선택합니다.'],
-                ['RC', '나는' + Constants.P2_codified_R + '를 선택합니다.']
+                ['LC', '나는 ' + Constants.P2_codified_L + '를 선택합니다.'],
+                ['RC', '나는 ' + Constants.P2_codified_R + '를 선택합니다.']
         ]
         if not self.group.ask_used:
             choices.append(
